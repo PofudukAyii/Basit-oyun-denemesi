ca6f294c971882d64c6faab7c4c4b38ca339d492
Pofuduk_Dev <kaan.erturkuner.00@gmail.com>
2022-04-23 19:48:04 +0300

    no message

--- oyun.py
+++ oyun.py
@@ -1,15 +1,15 @@
 import random
-from takvim import takvim_doğru_cevap
+from takvim import takvim_oyun
 
 
 oyun_türü = ["AlışVeriş", "İşlem", "YaşProblemi",
-             "Paragraf", "Problem1", "Problem 2", "Problem3", "Saat"]
+             "Paragraf", "Problem1", "Problem 2", "Problem3", "Saat", "Takvim"]
 
 seçilmiş_tür = random.choice(oyun_türü)
 print(seçilmiş_tür)
 
 if seçilmiş_tür == "Takvim":
-    takvim_doğru_cevap()
+    takvim_oyun()
 
 elif seçilmiş_tür != "Takvim":
     print("Takvim sorusu değil")
